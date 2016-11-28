77620480f2de1db50cd9601056825c49f29fc502
kiwiov <hugohansen97@gmail.com>
2016-11-28 13:42:58 +0100

    Added Tank.cs

---
+++ tank_mono/tank_mono/Tank.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace tank_mono
+{
+    class Tank
+    {
+        private Vector2 _position;
+
+        private string _tankType;
+
+        private float _health;
+        private float _armour;
+        private float _speed;
+        private float _fuel;
+
+        private Dictionary<Weapon, int> _weapons;
+
+        private Texture2D _spriteMain;
+        private Texture2D _cannon;
+
+
+        Tank(Vector2 Position, String TankType)
+        {
+            this.Position = Position;
+            this.TankType = TankType;
+            SetStats(this.TankType);
+        }
+
+        private void SetStats(string tankType)
+        {
+            switch (tankType)
+            {
+                case "Heavy":
+                    //Tank stats
+                    //Add weapons
+                    //Tank sprites
+                    break;
+                case "Standard":
+                    //tank stats
+                    //Add weapons
+                    //Tank sprites
+                    break;
+                case "Light":
+                    //tank stats
+                    //Add weapons
+                    //Tank sprites
+                    break;
+            }
+        }
+
+        public float Fuel
+        {
+            get { return _fuel; }
+            set { _fuel = value; }
+        }
+
+
+        public Texture2D Cannon
+        {
+            get { return _cannon; }
+            set { _cannon = value; }
+        }
+
+
+        public Texture2D SpriteMain
+        {
+            get { return _spriteMain; }
+            set { _spriteMain = value; }
+        }
+
+
+        public Dictionary<Weapon, int> Weapons
+        {
+            get { return _weapons; }
+            set { _weapons = value; }
+        }
+
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public float Armour
+        {
+            get { return _armour; }
+            set { _armour = value; }
+        }
+
+
+        public float Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+
+        public string TankType
+        {
+            get { return _tankType; }
+            set { _tankType = value; }
+        }
+        
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+    }
+}
